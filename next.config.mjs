1cabba6578af35ab435578089d4a00110da176cd
Demi-osi <demi@itthynk.co.za>
2025-01-15 15:43:17 +0200

    changed next config file for env variables

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,10 @@ const nextConfig = {
     output: 'standalone'
 };
 
+module.exports = {
+      env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+    };
+
 export default nextConfig;
