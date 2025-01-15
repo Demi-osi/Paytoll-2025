0de2980d993e927a35e6c24f4c41062441f3fec5
Demi-osi <demi@itthynk.co.za>
2025-01-15 15:53:03 +0200

    Clean up

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    env: {
-      DATABASE_URL: process.env.DATABASE_URL,
-    }
   };
 
 export default nextConfig;
